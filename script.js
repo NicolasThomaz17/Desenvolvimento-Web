a520082a6bb51e2098b8e5e1376eae0b1107af29
Nicolas Thomaz <nicolas.tsilvaa@gmail.com>
2025-09-25 11:45:36 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+console.log(i);
+    document.getElementById('btnEnviar').innerText = 'Enviar para NASA';
+for(let i = 0; i < 10000000; i ++){
+    console.log(i);
+    document.getElementById('btnEnviar').innerText = 'Enviar para NASA';
+    
+}
